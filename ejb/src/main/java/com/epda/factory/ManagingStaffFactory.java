fe841b61b5aff183d8c236a1e92373c7653ae186
aaron <aaronlyn88@gmail.com>
2024-03-08 09:07:29 +0800

    feat: added fake data generation for more user, added pre hook commit

---
+++ ejb/src/main/java/com/epda/factory/ManagingStaffFactory.java
@@ -0,0 +1,12 @@
+package com.epda.factory;
+
+import com.epda.model.ManagingStaff;
+
+public class ManagingStaffFactory {
+    public static ManagingStaff createManagingStaff() {
+        ManagingStaff staff = new ManagingStaff();
+        UserFactory.populateUserFields(staff);
+        // Additional specific fields for ManagingStaff can be set here
+        return staff;
+    }
+}
--- ejb/src/main/java/com/epda/services/DatabaseSeeder.java
+++ ejb/src/main/java/com/epda/services/DatabaseSeeder.java
@@ -4,6 +4,8 @@ import com.epda.model.Customer;
 import com.epda.model.Pet;
 import com.epda.factory.CustomerFactory;
 import com.epda.factory.PetFactory;
+import com.epda.factory.ManagingStaffFactory;
+import com.epda.factory.ReceptionistFactory;
 
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
@@ -25,6 +27,11 @@ public class DatabaseSeeder {
                 pet.setCustomer(customer); // Assuming Pet class has a setCustomer method
                 em.persist(pet);
             }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            em.persist(ManagingStaffFactory.createManagingStaff());
+            em.persist(ReceptionistFactory.createReceptionist());
         }
     }
 }
